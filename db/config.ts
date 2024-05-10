e6b2df697856c29720678896ddee5fbf2ad310c3
AryaC8 <160004534+AryaC8@users.noreply.github.com>
2024-05-10 13:10:18 +0200

    creacion base de datos

--- db/config.ts
+++ db/config.ts
@@ -1,6 +1,45 @@
-import { defineDb } from 'astro:db';
+import { defineDb, defineTable, column } from 'astro:db';
+
+const User = defineTable({
+  columns:{
+    id: column.text({primaryKey: true}),
+    nombre: column.text(),
+  }
+})
+
+const Cuento = defineTable({
+  columns:{
+    id: column.text({primaryKey: true}),
+    nombre: column.text(),
+    autor: column.text(),
+    editorial: column.text(),
+    anyo: column.text(),
+    categoria: column.text(),
+    add: column.text(), 
+    quien: column.text(),   
+    prestado: column.boolean(),
+    prestadoA: column.text()
+  }
+})
+
+const CuentoDeseado = defineTable({
+  columns:{
+    id: column.text({primaryKey: true}),
+    nombre: column.text(),
+    autor: column.text(),
+    editorial: column.text(),
+    anyo: column.text(),
+    categoria: column.text(),
+    add: column.text(), 
+    quien: column.text(),   
+  }
+})
 
 // https://astro.build/db/config
 export default defineDb({
-  tables: {}
+  tables: {
+    Cuento,
+    CuentoDeseado, 
+    User
+  }
 });
