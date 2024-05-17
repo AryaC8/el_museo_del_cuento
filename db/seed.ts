9d8075a0551eebdd043251b0719f129202df4b9e
AryaC8 <160004534+AryaC8@users.noreply.github.com>
2024-05-17 12:09:22 +0200

    boceto tabla categoría

--- db/seed.ts
+++ db/seed.ts
@@ -1,6 +1,8 @@
-// import { db } from 'astro:db';
+// import { db, Categoria } from 'astro:db';
 
 // // https://astro.build/db/seed
 // export default async function seed() {
-	
+	//await db.insert(Categoria).values([
+//       {id: 1, nombre: "Familiar"},
+//    ])
 // }
