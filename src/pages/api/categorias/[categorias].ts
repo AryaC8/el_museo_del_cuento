a06d8b98f10b06bfaf9d15e52dc95dfcc5b853c1
AryaC8 <160004534+AryaC8@users.noreply.github.com>
2024-05-25 11:38:31 +0200

    ver cuentos que corresponden a cada categoria

---
+++ src/pages/api/categorias/[categorias].ts
@@ -0,0 +1,21 @@
+import type { APIRoute } from "astro";
+import { db, Cuento, eq } from 'astro:db';
+
+
+export const POST: APIRoute = async ({ url }) =>{
+    const urlTotal =  url.pathname
+    const categoria = urlTotal.charAt(url.pathname.length-1)   
+    let cuento 
+    if(categoria)
+        cuento = await db.select({nombre : Cuento.nombre}).from(Cuento).where(eq (Cuento.categoria, categoria));
+    else 
+        return new Response (categoria, { status: 400 })
+
+    if(!cuento)return new Response ("Cuento no encontrado", { status: 404 })
+        
+    return new Response (
+        JSON.stringify({
+             cuento 
+        })
+    )
+}
